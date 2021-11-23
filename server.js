965732386caea6a7cc1724fbe6dd4edbebd5a5ee
Yingjia Liu <liuyjca@gmail.com>
2021-11-22 20:16:36 -0500

    Fixed error in server js code

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/problem', (req, res) => {
 // will use an 'environmental variable', process.env.PORT, for deployment.
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-	log(`Listening on port ${port}...`)
+	console.log(`Listening on port ${port}...`)
 })  // localhost development port 5000  (http://localhost:5000)
    // We've bound that port to localhost to go to our express server.
    // Must restart web server when you make changes to route handlers.
